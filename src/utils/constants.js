e8fb1e46870f8414328fe956b09163fe21001e1a
Palina Karabovich <polina.korobovich@yandex.by>
2022-11-20 15:10:08 +0100

    server url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const SERVER__URL = 'https://api.nomoreparties.co/';
 
-export const API_MAIN_URL = 'https://palinakarabovich.mesto.nomoredomains.icu';
+export const API_MAIN_URL = 'https://palinakarabovich.mesto.nomoredomains.icu/api';
 
 export const API_MOVIE_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
